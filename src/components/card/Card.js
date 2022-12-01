8884470da5aa7feb6c01acc3a4c4799905c3e2ee
unknown <appcampdev@gmail.com>
2022-12-01 22:45:00 +0500

    test Gamestore

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,21 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './card.css';
 
 function Card(props) {
     const [ cartSession, setCartSession ] = useState([]);
-    useEffect(() => {
-        console.log(cartSession);
-    }, [cartSession]);
     function test(event) {
-        if (event.target.id === 'notpressed') {
-            event.target.id = 'pressed'
-            event.target.innerHTML = 'убрать';  
-            setCartSession(cartSession.concat(props.id));     
-        } else {
-            event.target.id = 'notpressed'
-            event.target.innerHTML = 'в корзину';
-        }   
+        setCartSession(cartSession.concat(props.id));     
     }
     return (
         <div className="сard">
@@ -24,7 +14,11 @@ function Card(props) {
                 <div className="сard__container-title">{props.title}</div>
                 <div className="сard__container-descripton">{props.description}</div>
                 <div className="card__container-more"><Link to = { `game/` + props.id}>Подробнее</Link></div>
-                <div className="card__container-price">{props.price} <span id="notpressed" onClick={test} >в корзину</span></div>       
+                {cartSession.includes(props.id) ? (
+                    <div className="card__container-price">{props.price} <span onClick={test} >убрать</span></div> 
+                ) :(
+                    <div className="card__container-price">{props.price} <span onClick={test} >в корзину</span></div>
+                )}      
                 <div className="card__container-split">___</div>  
             </div>  
         </div>
